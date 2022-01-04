ed67a3c1f16f164f597574b2b2ba2776f2f6e8af
Ghorrid <dawid.kaminski83@gmail.com>
2022-01-04 19:17:41 +0100

    Add project files.

---
+++ PictureManager/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PictureManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbMainPicture = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tbPictureFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMainPicture
+            // 
+            this.pbMainPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbMainPicture.Location = new System.Drawing.Point(12, 103);
+            this.pbMainPicture.Name = "pbMainPicture";
+            this.pbMainPicture.Size = new System.Drawing.Size(667, 410);
+            this.pbMainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbMainPicture.TabIndex = 0;
+            this.pbMainPicture.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(93, 58);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Dodaj zdjęcie";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(588, 39);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(93, 58);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Wyczyść";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Visible = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tbPictureFileName
+            // 
+            this.tbPictureFileName.Location = new System.Drawing.Point(234, 77);
+            this.tbPictureFileName.Name = "tbPictureFileName";
+            this.tbPictureFileName.Size = new System.Drawing.Size(266, 20);
+            this.tbPictureFileName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nazwa pliku:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 525);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPictureFileName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.pbMainPicture);
+            this.MinimumSize = new System.Drawing.Size(707, 564);
+            this.Name = "Form1";
+            this.Text = "PictureManager";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMainPicture;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox tbPictureFileName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PictureManager/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureManager
+{
+    public partial class Form1 : Form
+    {
+
+        private string _savePath = Path.Combine(Environment.CurrentDirectory, "filepath.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string path = File.ReadAllText(_savePath);
+            if (path != null && path != "")
+            {
+                tbPictureFileName.Text = path;
+                pbMainPicture.Image = Bitmap.FromFile(path);
+                btnClear.Visible = true;
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files (*.jpg; *.bmp; *.jpeg; *.png)| *.jpg; *.bmp; *.jpeg; *.png";
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                tbPictureFileName.Text = ofd.FileName;
+                pbMainPicture.Image = Bitmap.FromFile(ofd.FileName);
+                btnClear.Visible = true;
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (pbMainPicture.Image != null)
+            {
+                pbMainPicture.Image = null;
+                tbPictureFileName.Text = null;
+                btnClear.Visible = false;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            File.WriteAllText(_savePath, tbPictureFileName.Text);
+        }
+
+   
+    }
+}
